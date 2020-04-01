f037a21bb89963d41f9e78436171d24a23d436f2
Eureudes Santos <eureudes@gmail.com>
2020-04-01 08:54:34 -0300

    inicial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function formatMoney(value) {
+
+}
+
+function formatCep(value) {
+    return value = String(value)
+        .replace(/\D/g, "")
+        .slice(0, 8)
+        .replace(/(\d{5})(\d)/, "$1-$2")
+}
+
+module.exports = {
+    formatMoney,
+    formatCep
+}
